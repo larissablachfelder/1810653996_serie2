15efc95fdfdda7f6cd84e9ffbe33a4bb9e9a670b
Larissa Blachfelder <1810653996@stud.fh-kufstein.ac.at>
2018-11-22 14:10:21 +0100

    aufgabe 1 mit kommentaren

--- src/serie2/Main.java
+++ src/serie2/Main.java
@@ -12,13 +12,13 @@ public class Main {
         //einen Integer einlesen und in einen Integer umwandeln, da JOptionPane gibt immer string zurück
         int alter = Integer.parseInt(JOptionPane.showInputDialog("Alter eingeben"));
 
-        if (alter<18)
-        {
+        if (alter<18) //überprüft die eingabe Alter, ob es kleiner 18 ist, wenn ja folgt das
+        {   //showMessageDialog braucht als parentComponent null; gibt Text im Dialogfeld aus wenn jünger als 18
             JOptionPane.showMessageDialog(null,"Du bist leider noch nicht volljährig.");
         }
 
-        else
-        {
+        else //ansonsten wird das ausgeführt
+        {   //ist man 18 oder älter wird dieser Text ausgegeben
             JOptionPane.showMessageDialog(null,"Du bist volljährig.");
         }
 
