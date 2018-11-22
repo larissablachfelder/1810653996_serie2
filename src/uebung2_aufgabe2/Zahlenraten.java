1ebed1ac1a1d609457130127aaa8d66cea66384d
Larissa Blachfelder <1810653996@stud.fh-kufstein.ac.at>
2018-11-22 15:55:10 +0100

    aufgabe 4

--- src/uebung2_aufgabe2/Zahlenraten.java
+++ src/uebung2_aufgabe2/Zahlenraten.java
@@ -37,6 +37,11 @@ public class Zahlenraten
 
         String eingabe = JOptionPane.showInputDialog("Bitte geben Sie eine Zahl zwischen 1 und 10 ein!");
 
+        /*das Vergleiche funktioniert nicht, weil JOptionPane immer einen String zurückliefert und man keinen String
+            mit einem Integer vergleichen kann, deshalb muss man JOptionPane vorher in einen Integer umwandeln mithilfe
+            des Befehls Integer.parseInt
+             */
+
 
         //string in integer umwandeln
 
@@ -47,38 +52,62 @@ public class Zahlenraten
         dann müsste in der if auch die variable eingabe mit zufallszahl verglichen werden
          */
 
-        if(eingabe1 == zufallszahl)
-        {
-            /*das funktioniert nicht, weil JOptionPane immer einen String zurückliefert und man keinen String
-            mit einem Integer vergleichen kann, deshalb muss man JOptionPane vorher in einen Integer umwandeln mithilfe
-            des Befehls Integer.parseInt
-             */
-
-            JOptionPane.showMessageDialog(null,"Die Zahlen stimmen überein.");
-        }
-
-        else if (eingabe1>=zufallszahl-2 && eingabe1<=zufallszahl+2)
-        {
-
-            JOptionPane.showMessageDialog(null, "Du bist innerhalb der Grenze von -2 und +2.");
-        }
-
-
-        else
-        {
-
-            JOptionPane.showMessageDialog(null, "Die Zahlen stimmen nicht überein.");
-        }
 
 
 
+
+
+
+
+         //aufgabe4
+         int versuche; //variable um versu
+         for (versuche=0; versuche<3; versuche++)
+         {
+
+             if (eingabe1 != zufallszahl) //beim nächsten mal vorher überprüfen, ob es richtig ist --> inhalt von else und if tausche und eingabe mit zufallszahl vergleichen
+             {
+                 if(eingabe1 == zufallszahl)
+                 {
+
+
+                     JOptionPane.showMessageDialog(null,"Die Zahlen stimmen überein.");
+                 }
+
+                 else if (eingabe1>=zufallszahl-2 && eingabe1<=zufallszahl+2)
+                 {
+
+                     JOptionPane.showMessageDialog(null, "Du bist innerhalb der Grenze von -2 und +2.");
+                 }
+
+
+                 else
+                 {
+
+                     JOptionPane.showMessageDialog(null, "Die Zahlen stimmen nicht überein.");
+                 }
+                 eingabe1 = Integer.parseInt(JOptionPane.showInputDialog("Gib erneut eine Zahl ein!"));
+
+             }
+
+             else
+             {
+                 JOptionPane.showMessageDialog(null, "Versuch zu Ende, du hast die Zahl " +zufallszahl + " erraten!");
+
+                 break;
+             }
+
+
+
+             //überprüfen ob alle 3 versuche schon verbraucht sind
+             if(versuche==2)
+             {
+                 JOptionPane.showMessageDialog(null, "Du hast alle Versuche verbraucht! Die richtige Zahl wäre " + zufallszahl + " gewesen.");
+             }
+
+
+         }
 
 
     }
-
-
-
-
-
 
 }
