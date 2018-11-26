e47f3abccd2e631dffcbbf54e8e756bb103e49af
Larissa Blachfelder <1810653996@stud.fh-kufstein.ac.at>
2018-11-26 14:31:01 +0100

    aufgabe 6

--- src/uebung2_aufgabe2/aufgabe6.java
+++ src/uebung2_aufgabe2/aufgabe6.java
@@ -3,7 +3,7 @@ package uebung2_aufgabe2;
 import javax.swing.*;
 import java.util.Random;
 
-public class aufgabe5 {
+public class aufgabe6 {
 
     public static void main(String[] args)
     {
@@ -17,11 +17,11 @@ public class aufgabe5 {
 
 
 
-        //aufgabe 5 version 1 ohne grenzen
+        //aufgabe 6 do-while-schleife
 
 
            int versuche = 0;
-                  while (versuche < 3)//hier wird überprüft, ob die schleife ausgeführt wird oder nicht
+                  do //braucht keinen parameter, da dieser später bei while dabeisteht, am ende der schleife wird die versuche variable erhöht
                   {
                       String eingabe = JOptionPane.showInputDialog("Bitte geben Sie eine Zahl zwischen 1 und 10 ein!");
                       int eingabe1 = Integer.parseInt(eingabe);
@@ -34,7 +34,7 @@ public class aufgabe5 {
 
                        else
                       {
-                             versuche++; //hier wird die variable erhöht
+                             versuche++;
                            if (versuche > 2)
                            {
                                JOptionPane.showMessageDialog(null, "Sie haben die Zahl nicht erraten. Die richtige Zahl wäre " + zufallszahl + " gewesen! Alle Versuche aufgebraucht!");
@@ -46,11 +46,20 @@ public class aufgabe5 {
                                 JOptionPane.showMessageDialog(null, "Sie haben die Zahl leider falsch :( Aber Sie können es noch " + (3 - versuche) + " mal versuchen. Viel Glück ");
                            }
                       }
+                      versuche++; //hier wird die variable erhöht
                    }
 
+                    while(versuche<3);//hier wird die variable &uuml;berpr&uuml;ft und anschlie&szlig;end entweder abgebrochen oder wiederholt
 
 
 
+                //do-while ist fußgesteuert, weil am ende der schleife erhöht und überprüft wird, ob die Schleife ausgeführt wird oder nicht
+
+                //das programm arbeitet nur mehr teilweise und wiederholt die schleife nicht mehr, nach einmal eintippen wird die schleife abgebrochen
+
+        //For-schleife: Zählvariable, zählschritte und höchstanzahl stehen in der BEdingung im Kopf
+        //While-schleife: Überprüfung im Kopf, variable wird im Körper erhöht
+        //DO_WHILE: zuerst wird ausgeführt und anschließend die variable erhöht und erst danach überprüft
 
 
 
